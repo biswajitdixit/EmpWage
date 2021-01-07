cc08f24f0c7ffd04bb95b9ff8377a2b14bd8b510
biswajitdixit <buntyoec1998@gmail.com>
2021-01-07 13:54:09 +0530

    CHANGED:Adding emp wage in case statement

--- empWage.sh
+++ empWage.sh
@@ -5,15 +5,16 @@ echo "Welcome to Employee Wage Computation Program"
 isPartTime=1
 isFullTime=2
 empRatePerHr=20
-randomCheck=$((RANDOM%3))
+empCheck=$((RANDOM%3))
 
-if [ $isFullTime -eq $randomCheck ]
-then
-     empHrs=8;
-elif [ $isPartTime -eq $randomCheck ]
-then
-     empHrs=4;
-else
-    empHrs=0;
-fi
+case $empCheck in
+        $isFullTime)
+               empHrs=8
+                 ;;
+        $isPartTime)
+               empHrs=4
+                 ;;
+                  *)
+                 ;;
+esac
 salary=$(( $empHrs * $empRatePerHr ))
